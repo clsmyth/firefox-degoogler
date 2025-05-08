6823b679c0d0559541d5d6d4e63c9ea7faf8b2b0
Data Bonanza <bling.status@blingstat.us>
2025-05-08 09:53:47 -0500

    adding some comments

--- firefox-degoogler.sh
+++ firefox-degoogler.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# check if we're running on a mac
 if [[ "$OSTYPE" =~ "darwin" ]]; then
     echo "Running on a Mac"
+    # set profile directory
     profile_dir=~/Library/Application\ Support/Firefox/Profiles/
+    # find individual profiles and loop through them
     find "${profile_dir}" -maxdepth 1 -mindepth 1 -type d| while read found_profile; do \
-      #found_profile=$(basename "${found_profile}"); \
       echo "copying user.js to ${found_profile}"; \
       # this WILL indiscriminately overwrite any existing user.js so we will take a backup.
       cp -R "${found_profile}"/user.js{,.$(date +%s).bak}; \
+      # copy the user.js file to the profile directory
       cp -R ./user.js "${found_profile}"; \
       echo "done"; \
     done
