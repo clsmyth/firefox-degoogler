ced80359758898381b2cec870aa04f6302841c73
Christian Smyth <clsmyth@gmail.com>
2025-05-08 11:13:14 -0500

    general refactoring:
    - rewrote firefox-degoogler.sh for clarity and safety.
    - renamed user.js to model_user.js .

--- firefox-degoogler.sh
+++ firefox-degoogler.sh
@@ -1,20 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # check if we're running on a mac
 if [[ "$OSTYPE" =~ "darwin" ]]; then
     echo "Running on a Mac"
-    # set profile directory
-    profile_dir=~/Library/Application\ Support/Firefox/Profiles/
-    # find individual profiles and loop through them
-    find "${profile_dir}" -maxdepth 1 -mindepth 1 -type d| while read found_profile; do \
-      echo "copying user.js to ${found_profile}"; \
-      # this WILL indiscriminately overwrite any existing user.js so we will take a backup.
-      cp -R "${found_profile}"/user.js{,.$(date +%s).bak}; \
-      # copy the user.js file to the profile directory
-      cp -R ./user.js "${found_profile}"; \
-      echo "done"; \
-    done
 else
     echo "Not running on a Mac"
+    exit 0
 fi
 
+# check if we have the model profile to use
+model_js="./model_user.js"
+if [ -f "${model_js}" ]; then
+    echo "Model profile '${model_js}' found"
+else
+    echo "Model profile '${model_js}' not found"
+    exit 1
+fi
 
+# check for profile directory
+profile_dir=~/"Library/Application Support/Firefox/Profiles"
+profile_dir=~/"fire fox"
+
+if [ -d "${profile_dir}" ]; then
+    echo "Profile dir '${profile_dir}' found"
+else
+    echo "Profile dir '${profile_dir}' not found"
+    exit 1
+fi
+
+# timestamp
+ts="$(date +%s)"
+
+# loop thru individual profiles and replace them with the model profile
+find "${profile_dir}" -type f -name user.js -print |
+    while read user_js; do
+        user_js_bak="${user_js}.${ts}.bak"
+        echo "backing up '${user_js}' to '${user_js_bak}'"
+        cp -af "${user_js}" "${user_js_bak}"
+        echo "overwriting '${user_js}' with model profile"
+        if cat "${model_js}" 1>"${user_js}" ; then
+            echo "done"
+        else
+            echo "FAIL"
+        fi
+    done
